f14e68a49292aeef44eaea8a89de516dea692211
Chimara Okeke <123244902+Echimara@users.noreply.github.com>
2024-02-12 18:31:29 -0600

    Rearrange Captions

--- Old_Phone_Validator.sql
+++ Old_Phone_Validator.sql
@@ -1,5 +1,5 @@
--- Uses IF-ELSE logic
 -- What it does do? Checks to see if a certain character or alphabet maps to a number of the phone pad 📱 
+-- Uses IF-ELSE logic
 -- DATE: 2/5/2024
 
     DO $GO$
